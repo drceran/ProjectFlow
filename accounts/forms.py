a459dd28c441f1aef26fa0f4a101a2c4dd7e01f6
Esra Ceran <dresraceran@gmail.com>
2023-03-07 11:12:40 -0800

    Feature 10 complete

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,3 +7,11 @@ from django.forms import ModelForm
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=150)
     password = forms.CharField(max_length=150, widget=forms.PasswordInput)
+
+
+class SignupForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(max_length=150, widget=forms.PasswordInput)
+    password_confirmation = forms.CharField(
+        max_length=150, widget=forms.PasswordInput
+    )
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-from accounts.forms import LoginForm
+from accounts.forms import LoginForm, SignupForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 
@@ -30,3 +32,28 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect("login")
+
+
+def user_signup(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            password_confirmation = form.cleaned_data["password_confirmation"]
+            if password == password_confirmation:
+                user = User.objects.create_user(username, password=password)
+                login(request, user)
+                return redirect("list_projects")
+            else:
+                form.add_error("the passwords do not match")
+
+    else:
+        form = SignupForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/signup.html", context)
+
+
+# form a error ekle, formu context e koyup ayni sayfaya gonder.
